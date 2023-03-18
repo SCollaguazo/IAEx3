79c9103b2d1c8f07e505b2a5e6bd07b3b7810610
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-18 17:32:10 -0500

    up5

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Examen 3 Collaguazo Santiago 🚅"})
+    return jsonify({"Examen"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
