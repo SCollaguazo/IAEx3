7b527bb3d12480194832895fec7a932b170f49cc
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-18 18:27:07 -0500

    cambio return

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Examen"})
+    #load models("modelo.h5")
+    #load_scaler("scaler.pkl")
+    #X=scaler.transform(X)
+    #predic=model.predict(X)[0]
+    #return jsonify({predic})
+    return jsonify({"Examen":"Santiago Collaguazo"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
