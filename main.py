ba5239486a6feab0e93b14a533a5e73ca9fec074
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-19 03:15:17 -0500

    test

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ def predecir():
     array = {"metar": [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
     request.json['skyc3_o'],request.json['wxcodes_o'],request.json['year'],request.json['month'],
     request.json['day'],request.json['hora'],request.json['minutos']]}
-    data = json.loads(array)
-    dList = data['metar']
-    model = keras.models.load_model("model.h5")
-    scaler = load(open('scaler.pkl', 'rb'))
-    scaList=scaler.transform(dList)
-    predic=model.predict(scaList)
-    return jsonify({"Examen":"Santiago Collaguazo","Label":predic})
+    return jsonify({"Examen":"Santiago Collaguazo","Label":array})
+    #data = json.loads(array)
+    #dList = data['metar']
+    #model = keras.models.load_model("model.h5")
+    #scaler = load(open('scaler.pkl', 'rb'))
+    #scaList=scaler.transform(dList)
+    #predic=model.predict(scaList)
+    #return jsonify({"Examen":"Santiago Collaguazo","Label":predic})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
