7d5d103efc4da3c14e8ee26a6ab5b7b5ba1eb291
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-19 03:59:47 -0500

    uno

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ def index():
 def predecir():
     #if not request.json:
         #abort(400)
+    
+    array = [request.json['tempd_o']
+             #,request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
+    #request.json['skyc3_o'],request.json['wxcodes_o'],request.json['year'],request.json['month'],
+    #request.json['day'],request.json['hora'],request.json['minutos']]}
         
-    array = {'metar': [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
-    request.json['skyc3_o'],request.json['wxcodes_o'],request.json['year'],request.json['month'],
-    request.json['day'],request.json['hora'],request.json['minutos']]}
+    #array = {'metar': [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
+    #request.json['skyc3_o'],request.json['wxcodes_o'],request.json['year'],request.json['month'],
+    #request.json['day'],request.json['hora'],request.json['minutos']]}
     return jsonify({"Examen":"Santiago Collaguazo","Label":array[0]})
     #data = json.loads(array)
     #dList = data['metar']
