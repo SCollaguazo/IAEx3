e3e5a1413d10b8e17d232176fc004e11d28f2b88
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-19 02:54:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def index():
 
 @app.route('/',methods=['POST'])
 def predecir():
-    if not request.json:
-        abort(400)
+    #if not request.json:
+    #    abort(400)
     array = '{"metar": [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],
         request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],request.json['skyc3_o'],request.json['wxcodes_o'],
         request.json['year'],request.json['month'],request.json['day'],request.json['hora'],request.json['minutos']]}'
