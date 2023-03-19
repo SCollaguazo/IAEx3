d65c2d7d04b8dc2f9aa400f3a19284062b6e14e0
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-19 03:33:55 -0500

    arr

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def predecir():
     #if not request.json:
         #abort(400)
         
-    array = '{"metar": [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
+    array = '[{"metar": [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
     request.json['skyc3_o'],request.json['wxcodes_o'],request.json['year'],request.json['month'],
-    request.json['day'],request.json['hora'],request.json['minutos']]}'
+    request.json['day'],request.json['hora'],request.json['minutos']]}]'
     return jsonify({"Examen":"Santiago Collaguazo","Label":array})
     #data = json.loads(array)
     #dList = data['metar']
