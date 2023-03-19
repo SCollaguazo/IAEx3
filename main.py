568bdd13d0aaa53ca8dbc234774b56ccbcf5989f
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-19 04:03:36 -0500

    corchete final

--- main.py
+++ main.py
@@ -14,15 +14,16 @@ def predecir():
     #if not request.json:
         #abort(400)
     
-    array = [request.json['tempd_o']
-             #,request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
+    array = [request.json['tempd_o']]
+    return jsonify({"Label":array})
+    #,request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
     #request.json['skyc3_o'],request.json['wxcodes_o'],request.json['year'],request.json['month'],
     #request.json['day'],request.json['hora'],request.json['minutos']]}
         
     #array = {'metar': [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],
     #request.json['skyc3_o'],request.json['wxcodes_o'],request.json['year'],request.json['month'],
     #request.json['day'],request.json['hora'],request.json['minutos']]}
-    return jsonify({"Label":array})
+    
     #data = json.loads(array)
     #dList = data['metar']
     #model = keras.models.load_model("model.h5")
