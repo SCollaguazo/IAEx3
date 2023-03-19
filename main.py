222e3275bdb2110bcdc10dc8fe19e9b8527d0124
Santiago Collaguazo <60185790+SCollaguazo@users.noreply.github.com>
2023-03-19 02:21:36 -0500

    con json

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #load models("modelo.h5")
-    #load_scaler("scaler.pkl")
-    #X=scaler.transform(X)
-    #predic=model.predict(X)[0]
-    #return jsonify({predic})
     return jsonify({"Examen":"Santiago Collaguazo"})
+
+@app.route('/app',methods=['POST'])
+def predecir():
+    if not request.json:
+        abort(400)
+    array = '{"metar": [request.json['tempd_o'],request.json['rh_o'],request.json['dir_o'],request.json['spd_o'],request.json['mslp_o'],
+        request.json['visibility_o'],request.json['skyc1_o'],request.json['skyc2_o'],request.json['skyc3_o'],request.json['wxcodes_o'],
+        request.json['year'],request.json['month'],request.json['day'],request.json['hora'],request.json['minutos']]}'
+    data  = json.loads(array)
+    list = data['metar']
+    model = keras.models.load_model("model.h5")
+    scaler = load(open('scaler.pkl', 'rb'))
+    scaList=scaler.transform(list)
+    predic=model.predict(scaList)
+    return jsonify({"Examen":"Santiago Collaguazo",{"Label":predic}})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
